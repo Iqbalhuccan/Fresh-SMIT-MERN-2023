509280d8a5e99af57f8afd5c062c57e8ca9a0407
Iqbal huccan <ikbalkaan001@gmail.com>
2023-08-09 21:06:23 +0500

    JavaScript-assignment 1

---
+++ JavaScript-assignment-1/app.js
@@ -0,0 +1,272 @@
+// Assignment # 1: 
+
+                                        // CHapter # 1: Alerts: 
+// Q # 1
+// alert("Hello Pakistan")
+
+// Q # 2
+// alert("Error! Please enter a valid password.")
+
+// Q # 3: Use line break
+// alert("Welcome to JS Land...\nHappy Coding!")
+
+// Q # 4: 
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \nPrevent this page from creating additional dialogs.")
+
+// Q # 5:
+
+// Q # 6:
+// use of alerts in your HTML file
+
+// Q # 7:
+// Practice placement;  
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+
+                                            // Chapter # 2: VARIABLES FOR STRINGS
+// Q # 1
+// var username = "Iqbal Hussain"
+
+// Q # 2
+// var myName = "Muhammad Iqbal Hussain"
+
+// Q # 3
+// var message = "Hello World"
+// alert(message)
+
+// Q # 4
+// var StudentName = "Iqbal Hussain"
+// var StudentAge = "23 Years old"
+// var StudentCourse = "Web & Mobile App Development"
+
+// alert( "Student Name is \n" +StudentName)
+// alert("He is \n" +StudentAge)
+// alert("He is enrolled in \n" +StudentCourse)
+
+// Q # 5
+// var FastFood = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(FastFood);
+
+// Q # 6
+// var email= "ikbalkaan001@gmail.com"
+// alert("My Email Address is " + email)
+
+// Q # 7
+// var book ="A Smarter Way to Learn JavaScript"
+// alert("I am trying to learn from the Book " + book)
+
+// Q # 8
+// in the html file
+// var text = " Yah, I can write HTML"
+// document.write("yah, I can write HTML")
+
+// Q # 9
+// var symbole = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(symbole)
+
+
+                                    // Chapter # 3: VARIABLES FOR NUMBERS
+// Q # 1
+// var age = "23"
+// alert( "I am " + age + " years old");
+
+// Q # 2
+// var visit = 1
+// alert("You have visited this site " + visit + " times");
+// visit = visit + 1
+// alert("You have visited this site " + visit + " times");
+// visit = visit + 1
+// alert("You have visited this site " + visit + " times");
+// visit = visit + 1
+// alert("You have visited this site " + visit + " times");
+// visit = visit + 1
+// alert("You have visited this site " + visit + " times");
+// visit = visit + 1
+// alert("You have visited this site " + visit + " times");
+// visit = visit + 1
+// alert("You have visited this site " + visit + " times");
+
+// Q # 3
+// var birthYear = "1999";
+// document.write("My birth year is " + birthYear + " <br> Data type of my declared variable is number");
+
+// Q # 4
+// var VisitorName = "Iqbal"
+// var ProductTitle = "5 T-shirts"
+// var quantity = "Unofficial"
+// document.write(VisitorName + " ordered " + ProductTitle + " on " + quantity + " clothing store")
+
+
+                                            // Chapter # 4 
+//  Q # 1
+// var a = 10; b = 20; c = 30;
+// alert( a + b);
+// alert( b + c );
+// alert( c + a );
+
+//  Q # 2
+//  document.write("<h1>Variable illigal Names:</h1>")
+//  document.write("The name can't be a number or start with a number.<br>");
+//  document.write("It can't be any of JavaScript's keywords.<br>");
+//  document.write("Variable names are case sensitive. A rose is not a Rose.<br>")
+//  document.write("You can't enclose variable name in quotation marks.<br>")
+//  document.write("A variable name can't contain any spaces.<br><br><br>");
+
+//  document.write("<h1>Variable ligal Names:</h1>")
+//  document.write("A variable name can contain only letters, numbers, dollar signs, and underscores.<br>");
+//  document.write("The value of varibale should be written in camelCase.<br>");
+//  document.write("A camelCase name begins in lower case.<br>")
+//  document.write("The name can contain a number in the middle or in the last of variable.<br>")
+//  document.write("A variable name can contain hyphen or underscore.<br>");
+
+//  Q # 3
+// document.write("<h1>Rules for naming JS variables</h1><br>");
+// document.write("Variable names can only contain numbers, $ and _, , For example $my_1stVariable<br>");
+// document.write("Variables must begin with a letter, $,_. For example $name, _name or name<br>");
+// document.write("Variable names are case sensitive <br>");
+// document.write("Variable names should not be JS");
+
+
+                                    // Chapter # 5: MATH EXPRESSIONS
+
+//  Q # 1
+// var num1 = 10;
+// var num2 = 5;
+// var addNum = num1 + num2
+// document.write( "Sum of " + num1 + " and " + num2 + " is " + addNum);
+
+// //  Q # 2
+// var substractNum = num1 - num2
+// document.write( "<br> Substraction of " + num1 + " and " + num2 + " is " + substractNum);
+
+// var multiplyNum = num1 * num2
+// document.write( "<br> Product of " + num1 + " and " + num2 + " is " + multiplyNum);
+
+// var divideNum = num1 / num2
+// document.write( "<br> Division of " + num1 + " and " + num2 + " is " + divideNum);
+
+// var modulusNum = num1 % num2
+// document.write( "<br> Remainder of " + num1 + " and " + num2 + " is " + modulusNum);
+
+//  Q # 3
+var initialNum = 5
+var increment = initialNum++
+var addition = increment + 6
+var decrement = addition--
+var modulusNum = decrement % 2
+document.write("“Value after variable declaration is:" + "“Initial value: " + )
+
+//  Q # 4
+// var numOfTicket = 5
+// var ticketPrice = 600
+// var totalPrice = numOfTicket * ticketPrice
+// document.write("Total cost to buy " + numOfTicket + " tickets to a movie is " + totalPrice + "PKR");
+
+//  Q # 5: Table
+// var num = 5
+// var a = 1; b = 2; c = 3; d = 4; e = 5; f = 6; g = 7; h = 8; i = 9; j = 10;
+// var product = num * a
+// document.write("Table of  " + num + "<br>" + num +  " * " + a + " = " + product )
+// var product = num * b
+// document.write("<br>" + num +  " * " + b + " = " + product );
+// var product = num * c
+// document.write("<br>" + num +  " * " + c + " = " + product );
+// var product = num * d
+// document.write("<br>" + num +  " * " + d + " = " + product );
+// var product = num * e
+// document.write("<br>" + num +  " * " + e + " = " + product );
+// var product = num * f
+// document.write("<br>" + num +  " * " + f + " = " + product );
+// var product = num * g
+// document.write("<br>" + num +  " * " + g + " = " + product );
+// var product = num * h
+// document.write("<br>" + num +  " * " + h + " = " + product );
+// var product = num * i
+// document.write("<br>" + num +  " * " + i + " = " + product );
+// var product = num * j
+// document.write("<br>" + num +  " * " + j + " = " + product );
+
+//  Q # 6: Temperature Converter:
+// var celsius = 100
+// var celsius_to_fahrenheit = (celsius * 9/5) +32
+// document.write( celsius + " C is " + celsius_to_fahrenheit + " F.<br>");
+
+// var fahrenheit = 200
+// var fahrenheit_to_celsius = (fahrenheit -32) * 5/9
+// document.write( fahrenheit + " F is " + fahrenheit_to_celsius + " C.");           
+
+
+// Q # 7:
+// var price_item1 = 650
+// var price_item2 = 100
+// var item1_qty = 1
+// var item2_qty = 1
+// var shipping_charges = 100
+// var total_cost = (price_item1 * item1_qty) + (price_item2 * item2_qty) + shipping_charges
+
+// document.write("<br> Price of item 1 is " + price_item1 + "<br>" + "Quantity of item 1 is " + item1_qty);
+// document.write("<br> Price of item 2 is " + price_item2 + "<br>" + "Quantity of item 2 is " + item2_qty + " <br> Shipping Charges" + shipping_charges + "<br>");
+// document.write( "<br>" + "Total cost of your order is " + total_cost);
+
+// Q # 8:
+// var total_marks = 980
+// var obtained_marks = 804
+// var percentage = obtained_marks / total_marks * 100
+// document.write( "<h1> Marks Sheet </h1> <br>" + "Total Marks: " + total_marks + "<br> Marks Obtained: " 
+// + obtained_marks + " <br> Percentage: " + percentage + "%" )
+
+// Q # 9:
+// var USD = 20
+// var SR = 25
+// var exchange_to_pakistani = (USD * 280) + (SR * 75);
+// document.write("<h1> Currency in PKR </h1><br><br> Total Currency in PKR: " + exchange_to_pakistani)
+
+// Q # 10:
+// var num = 2
+// var result = ((num + 5) * 10 ) /2
+// document.write("The Result is = " + result)
+
+// Q # 11:
+// var currentYear = 2023
+// var birthYear = 1999
+// var yourAge = currentYear - birthYear
+// document.write("Current Year: " + currentYear + "<br> Birth Year " + birthYear + "<br> Your Age is : " + yourAge)
+
+// Q # 12:
+// var radius = 20
+// var circumference = 2 * 3.142 * radius
+// var area = 3.142 * radius **2
+// document.write("<h1>The Geometrizer</h1>"+ "Radius of a circle: " + radius + " <br> The Circumference is: " + circumference + 
+// " <br> The Area is: " + area)
+
+// Q # 13:
+var favSnack = "Lays"
+var currentAge = 24
+var maxAge = 50 
+var amountPerDay = 2
+var lifeTime = (maxAge - currentAge) * 365 * amountPerDay
+document.write("<h1> The Lifetime Supply Calculator</h1>" + "Favourite Snack: " + favSnack + "<br> Current Age: "
++ currentAge + " <br> Estimated Maximum Age: " + maxAge + "<br> Amount of Snacks per day: " + amountPerDay + "<br><br>")
+document.write("You will need " + lifeTime + " " +favSnack + " to last you until the ripe old age of " + maxAge)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
