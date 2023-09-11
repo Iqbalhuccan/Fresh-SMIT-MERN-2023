51d9b8b4d2b42828d764ff4e2abf0747988e1643
Iqbal huccan <ikbalkaan001@gmail.com>
2023-09-11 20:27:57 +0500

    JS assignment 5 added

---
+++ JavaScript-assignment-5/app.js
@@ -0,0 +1,74 @@
+// JavaScript Assignment-5 
+// Chapter 26 -30
+// Q #1:
+// var num = prompt("Enter a number:");
+// document.write( "number: "+ num + "<br/>" + "round off value: " + Math.round(num) + "<br/>" + 
+// "floor value: " + Math.floor(num) + "<br/>" + "Ceil value: " + Math.ceil(num))
+
+// Q #2:
+// var num = prompt("Enter a number:");
+// document.write("number: " + num + "<br/>" + "round off value: " + Math.round(num) + "<br/>" + "floor value: " 
+// + Math.floor(num) + "<br/>" + "Ceil value: " + Math.ceil(num))
+
+// Q #3:
+//  var num = prompt("Enter a number: ");
+//  alert( "The absolute value "+ num + " is " + Math.abs(num));
+
+// Q #4:
+// var num = Math.random();
+// var num1 = (num * 6);
+// var impNum = Math.round(num1)
+// document.write( "randome dice value: " + impNum)
+
+// Q #5:
+// var num = Math.random();
+// var num1 = (num * 2);
+// var impNum = Math.round(num1)
+// var improvedNum = Math.floor(impNum)
+// if(improvedNum ==2){
+//     document.write(improvedNum + "<br/>" +"random coin value: Head" )
+// }
+// else{
+//     document.write( improvedNum + "<br/>"+"random coin value: Tail")
+// }
+
+// Q #6:
+// var num = Math.random();
+// var num1 = (num * 100);
+// var rNum = Math.round(num1)
+
+// document.write( "Random number between 1 and 100:  "+rNum)
+
+// Q #7:
+
+// var weight = +prompt("Enter your weight in kilograms")
+// if( weight >= 1){
+// document.write("The weight of user is " + weight + " kilograms")
+// }
+// else{
+//     document.write("The weight of user is " + weight + " kilograms")
+// }
+
+// Q # 8:
+
+// var num = +prompt("Enter a number between 1 and 10: ");
+// var num1 = (num * 10)
+// var rNum = Math.round(num1)
+// if(num1 <= 10){
+//     alert("Congratulates")
+// }else{
+//     alert("Try again")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
