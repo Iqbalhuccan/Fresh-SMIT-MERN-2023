1f1705a7bf222e43c639d8ac57da4c1b6d38605a
Iqbal huccan <ikbalkaan001@gmail.com>
2023-09-18 21:19:17 +0500

    JS assignment of calculator added

---
+++ JavaScript-assignment-7/app.js
@@ -0,0 +1,17 @@
+
+
+function abc(){
+    var str = "5+5-";
+    var newInput = "";
+    var last = str[str.length - 1];
+    var arr = ["+", "-", "*", "/"];
+    
+    if(arr.indexOf(last) !== -1 && arr.indexOf(newInput) !== -1){
+        str =str.slice(0,-1) + newInput;
+    }
+    else{
+        str += newInput;
+    }
+    return str;
+    }
+    
